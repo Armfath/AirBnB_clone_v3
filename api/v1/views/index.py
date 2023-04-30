d07c7264535c1f205477e6bdad4bb516bb6da30b
Armfath <info.armanagonou@gmail.com>
2023-04-30 09:16:40 +0100

    add route stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """v1 starting point"""
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
@@ -8,3 +9,17 @@ from flask import jsonify
 def get_status():
     """Returns v1 status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Returns storage stats"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
